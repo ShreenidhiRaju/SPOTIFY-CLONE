1c7b7a81e04061dcb3f48a0cd5fb978a71e367af
ShreenidhiRaju <145311641+ShreenidhiRaju@users.noreply.github.com>
2025-07-13 11:48:52 +0530

    spotify clone

--- script.js
+++ script.js
@@ -19,8 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder){
     currFolder=folder
-    // let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
-    let a = await fetch(`/${folder}/`)
+    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -70,9 +69,7 @@ const playMusic=(track,pause=false)=>{
     
 }
 async function displayAlbums() {
-    let a = await fetch("/songs/manifest.json")
-    // let a = await fetch(`/songs/`)
-    // let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let a = await fetch(`http://127.0.0.1:3000/songs/`)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -84,8 +81,7 @@ async function displayAlbums() {
         if(e.href.includes("/songs")){
             let folder=e.href.split(`/`).slice(-2)[0]
             // get metadata of folder
-            let a = await fetch(`/songs/${folder}/info.json`)
-            // let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
             let response = await a.json()
             cardContainer.innerHTML=cardContainer.innerHTML+`<div data-folder="${folder}" class="card">
                         <div class="play">
